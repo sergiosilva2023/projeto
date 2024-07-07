a794e27e9432c6f1794e0954992bcf29c9045d47
Sérgio Silva <sergio.web100@gmail.com>
2024-07-08 00:12:13 +0100

    Usuario Controller

--- src/main/java/com/devssilva/projeto/controller/UsuarioController.java
+++ src/main/java/com/devssilva/projeto/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,8 @@ import com.devssilva.projeto.service.UsuarioService;
 
 
 @RestController
-@RequestMapping( value = "/usuario" ) 
+@CrossOrigin
+@RequestMapping( value = "api/usuario" ) 
 public class UsuarioController {
 	
 	@Autowired
